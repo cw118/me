9c9ce731e0c9f9125885329ce7fdcfb7511141b2
cw118 <carolyn94118@gmail.com>
2022-08-16 23:07:05 +0200

    Continuing work on Home (SeeAlso)

--- components/home/PageCard.js
+++ components/home/PageCard.js
@@ -1,21 +1,41 @@
-import { BsPersonCircle } from 'react-icons/bs';
-import { FaLaptopCode } from 'react-icons/fa';
-import { RiMailSendLine } from 'react-icons/ri';
+import styles from '../../styles/Home.module.css';
+import Link from 'next/link';
 
-export const AboutCard = () => {
-  return (
-    <BsPersonCircle />
+export const AboutCard = ({ lang }) => {
+  return ( 
+    <div className={styles.pageCard}>
+      <Link href={(lang ? '/fr' : '') + '/about'}><a>
+        <h2 className={styles.cardTitle}>{lang ? 'À propos' : 'About'}</h2>
+        <img src='/icons/about.svg' className={styles.pageIcon} />
+        <h3 className={styles.cardDesc}>{lang ? 'My tech stack and interests' : 'Ma stack technique et mes intérêts'}</h3>
+      </a></Link>
+    </div>
+    
   );
 }
 
-export const ProjectsCard = () => {
+export const ProjectsCard = ({ lang }) => {
   return (
-    <FaLaptopCode />
+    <div className={styles.pageCard}>
+      <Link href={(lang ? '/fr' : '') + '/projects'}><a>
+        <h2 className={styles.cardTitle}>{lang ? 'Projets' : 'Projects'}</h2>
+        <img src='/icons/projects.svg' className={styles.pageIcon} />
+        <h3 className={styles.cardDesc}>{lang ? 'Past and ongoing creations' : 'Exemples de mes réalisations'}</h3>
+      </a></Link>  
+    </div>
+    
   );
 }
 
-export const ContactCard = () => {
+export const ContactCard = ({ lang }) => {
   return (
-    <RiMailSendLine />
+    <div className={styles.pageCard}>
+      <Link href={(lang ? '/fr' : '') + '/contact'}><a>
+        <h2 className={styles.cardTitle}>Contact</h2>
+        <img src='/icons/contact.svg' className={styles.pageIcon} />
+        <h3 className={styles.cardDesc}>{lang ? 'Get in touch with me' : 'Me contacter'}</h3>
+      </a></Link>
+    </div>
+    
   );
 }
