5f0d559f25fe221cfd6e981a6a1116de361e2954
cw118 <carolyn94118@gmail.com>
2022-08-16 18:12:37 +0200

    Complete initial navbar

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,9 @@
 import { useRouter } from 'next/router';
 import styles from '../styles/Nav.module.css';
 import Link from 'next/link';
+import NavIcon from './NavIcon';
 
-const Nav = ({ lang, i18nUrl }) => {
+const Nav = ({ lang }) => {
   const router = useRouter();
   const currentRoute = router.pathname;
   const navLinks = [
@@ -31,20 +32,22 @@ const Nav = ({ lang, i18nUrl }) => {
   return (
     <nav className={styles.topnav}>
       <div className={styles.profile}>
-        <Link href={lang ? '/fr/' : '/'}><>
-          <img id={styles.profileIcon} src="/profile.png" alt="cw118" />
-          <p>Carolyn Wu</p>
-        </></Link>
+        <Link href={lang ? '/fr/' : '/'} passHref>
+          <a><img id={styles.profileIcon} src="/profile.png" alt="cw118" /></a>
+        </Link>
+        <Link href={lang ? '/fr/' : '/'} passHref><a className={styles.name}>Carolyn Wu</a></Link>
         <button id={styles.changeLang}>
-          <Link href={i18nUrl}>
-            <div id="langWrapper">
-              <span id={styles.mobileLang}>{lang ? 'EN' : 'FR'}</span><span id={styles.lang}>{lang ? 'English' : 'Français'}</span>
-            </div>
+          <Link href={(lang ? '/' : '/fr') + currentRoute}>
+            <a>
+              <div id={styles.langWrapper}>
+                <span id={styles.mobileLang}>{lang ? 'EN' : 'FR'}</span><span id={styles.lang}>{lang ? 'English' : 'Français'}</span>
+              </div>
+            </a>
           </Link>
         </button>
       </div>
       <div className={styles.navLinks}>
-        {navLinks.map((navLink) => <Link href={(lang ? '/fr/' : '') + navLink.route}><a className={(currentRoute == navLink.route || currentRoute == '/fr' + navLink.route) ? `${styles.active}` : ''}>{lang ? navLink.fr : navLink.title}</a></Link>)}
+        {navLinks.map((navLink) => <Link href={(lang ? '/fr/' : '') + navLink.route}><a className={`${styles.navLink}` + ((currentRoute == navLink.route || currentRoute == '/fr' + navLink.route) ? ` ${styles.active}` : '')}><NavIcon link={navLink.title} /><span className={styles.linkPage}>{lang ? navLink.fr : navLink.title}</span></a></Link>)}
       </div>
     </nav>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Nav from '../components/Nav';
 export default function Home() {
   return (
     <>
-      <Nav i18nUrl='/fr/' />
+      <Nav />
     </>
   );
 }
